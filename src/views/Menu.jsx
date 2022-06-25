4e8b4a100d6fdc4779fef145619190fa87b982ce
Rene Ramirez <rdejramirez@gmail.com>
2022-06-25 18:51:27 -0500

    menu version beta finished

--- src/views/Menu.jsx
+++ src/views/Menu.jsx
@@ -1,5 +1,47 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
 const Menu = () => {
-  return <h2> Menu </h2>;
+
+    const Pannel = styled.nav`
+        position: absolute;
+        top: 55%;
+        left: 50%;
+        transform: translate(-50%);
+        width: 60vw;
+        height: 35vh;
+        background: rgba(210, 210, 230, 0);
+        border-radius: 10px ;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .opc {
+            margin: 2rem auto;
+            font-weight: bold;
+            color: white;
+            text-decoration: none;
+            background: rgba(20, 140, 200, .96);
+            width: 10rem;
+            height: 6rem;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 5px;
+            transition: 1s;
+            &:hover {
+                font-size: 2rem;
+                width: 20rem;
+                height: 10rem;
+            }
+        }
+    `
+
+  return (
+    <Pannel>
+        <Link to="/cards" className="opc"> Nuevo Juego </Link>
+    </Pannel>
+  )
 };
 
 export default Menu;
